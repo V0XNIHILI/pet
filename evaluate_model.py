73b9ffc5636c6642e017b69b8b77895476eaee0e
Alin Dondera <a.dondera@student.tudelft.nl>
2022-03-23 17:13:11 +0100

    add progress bar and max size parameter

--- evaluate_model.py
+++ evaluate_model.py
@@ -5,6 +5,7 @@ import os
 import torch
 from transformers import AutoModelForSequenceClassification, AutoTokenizer
 from sklearn.metrics import classification_report
+from tqdm import tqdm
 
 
 def create_train_test_data(split_percentage=0.2):
@@ -21,7 +22,7 @@ def create_train_test_data(split_percentage=0.2):
 
 train_data, test_data = create_train_test_data()
 
-label_names = train_data.columns
+label_names = test_data.columns[2:]
 
 if not os.path.exists("data/mftc/train.csv"):
     train_data.to_csv("data/mftc/train.csv")
@@ -31,9 +32,12 @@ if not os.path.exists("data/mftc/test.csv"):
 
 parser = argparse.ArgumentParser(description="CLI for evaluating a model on MFTC")
 
-parser.add_argument("--path", default="bert-base-uncased", type=str, required=True,
+parser.add_argument("--path", default=None, type=str, required=True,
                     help="The location of the model dir. Should contain a config.json file. If not specified it "
                          "defaults to the base pretrained model")
+
+parser.add_argument("--max-test-size", default=len(test_data), type=int, required=False,
+                    help="The number of test points to evaluate on.")
 
 args = parser.parse_args()
 
@@ -43,11 +47,15 @@ tokenizer = AutoTokenizer.from_pretrained('bert-base-uncased')
 
 y_predicted = []
 y_true = []
+
+test_data = test_data.to_numpy()[:args.max_test_size]
+
 with torch.no_grad():
-    for data_point in test_data.to_numpy():
+    for i in tqdm(range(len(test_data))):
+        data_point = test_data[i]
         encoded_input = tokenizer(data_point[1], return_tensors='pt')
         output = loaded_model(encoded_input['input_ids'], attention_mask=encoded_input['attention_mask'])
         y_predicted.append((output.logits.numpy().squeeze() >= 0).astype(np.int64))
         y_true.append(data_point[2:].astype(np.int64))
 
-print(classification_report(np.array(y_true), np.array(y_predicted)))
+print(classification_report(np.array(y_true), np.array(y_predicted), target_names=label_names))
